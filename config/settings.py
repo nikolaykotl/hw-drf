64abb0f7c50e87e8d1cfa1096dd37dc08afdd393
nikolaykotl <nikkot1981@gmail.com>
2024-01-26 20:20:41 +0400

    дз к уроку 27.2 изменения в settings.py

--- config/settings.py
+++ config/settings.py
@@ -84,18 +84,27 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'school',
+#        'USER': 'postgres',
+#        'HOST': '127.0.0.1',
+#        'PORT': 5432,
+#        'PASSWORD': '9501718367'
+#
+#    }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'school',
+        'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
-        'PASSWORD': '9501718367'
-
+        'PASSWORD': 'mysecretpassword',
+        'HOST': 'db'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -149,8 +158,8 @@ REST_FRAMEWORK = {
     ]
 }
 
-STRIPE_SECRET_KEY = 'sk_test_51ONH6GHovNnm9EPfrzlsQiKzdZI2dUjI8duMjL5iQUDz38udmfI8prT087BYodPQuajNNfWP7AI54FUDvgIC7WgP00QBNyWSe6'
-STRIPE_WEBHOOK_SECRET = 'whsec_3ea9a35f94f7e747ea620a5f0210effe3cf0d6b28c7f562dbaa75a376ae5a295'
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WEBHOOK_SECRET = os.getenv('STRIPE_WEBHOOK_SECRET')
 
 CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
